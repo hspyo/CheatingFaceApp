4264fd317f95abf26189737222fffb4d93e4e36a
Pyo <olok95@naver.com>
2020-09-02 13:25:52 +0900

    ADD Css nav, home, about

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 function showPreview(event) {
+  document.getElementsByClassName("upload__form").hidden = false;
   if(event.target.files.length > 0) {
     var src = window.URL.createObjectURL(event.target.files[0]);
     var preview = document.getElementById("preview-image");
+    var form = document.getElementsByTagName("label");
     preview.src = src;
     preview.style.display = "block";
+    form[0].style.display = "none";
   }
 }
+
