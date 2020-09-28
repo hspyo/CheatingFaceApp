addc88c9ac9c847de0bab657666dedf5a4f1634d
hspyo <olok95@naver.com>
2020-09-29 02:36:33 +0900

    UPDATE Detail touch (주석, css 네이밍 컨벤션정리, 전체 코드 정리)

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
-// Navbar Toggle Button
+// 네비게이션 토글 버튼
 const navMenu = document.querySelector(".nav__menu");
 const navToggleBtn = document.querySelector(".nav__toggle__btn");
 navToggleBtn.addEventListener("click", () => {
   navMenu.classList.toggle("open");
 });
 
-// Remove Menu list after clicking on
+// 메뉴 클릭 후 닫아주기
 document.querySelectorAll(".nav__menu__list").forEach((item) => {
   item.addEventListener("click", () => {
     navMenu.classList.remove("open");
   });
 });
 
-// Check Gender
+// 성별 체크
 function checkGender() {
-  const checkedGender = document.getElementsByClassName(
-    "gender__toggle-state"
-  )[0].checked;
+  const checkedGender = document.querySelector(".gender__state").checked;
 
+  // 토글버튼이 체크되어있으면 true 여성, 체크되어있지 않으면 false 남자
   if (checkedGender) {
     return "female";
   } else {
@@ -25,21 +24,22 @@ function checkGender() {
   }
 }
 
-// Show Image Preview
+// 이미지 프리뷰
 function showPreview(event) {
-  document.getElementsByClassName("upload__form").hidden = false;
-  let loader = document.querySelector(".loader");
-  let loadingMsg = document.querySelector(".loading__msg");
+  const loader = document.querySelector(".loader");
+  const loadingMsg = document.querySelector(".loading__msg");
+
   if (event.target.files.length > 0) {
     let src = window.URL.createObjectURL(event.target.files[0]);
-    let preview = document.getElementById("preview-image");
-    let form = document.getElementsByClassName("upload__label")[0];
+    let preview = document.querySelector(".preview__image");
+    let uploadForm = document.getElementsByClassName("upload__label")[0];
     preview.src = src;
     preview.style.display = "block";
-    form.style.display = "none";
+    uploadForm.style.display = "none";
     loader.style.display = "block";
     loadingMsg.style.display = "block";
   }
+  // 이미지 가져온 후 이미지 분석 시작 (티처블 머신 실행)
   init().then(() => {
     loader.style.display = "none";
     loadingMsg.style.display = "none";
@@ -47,15 +47,15 @@ function showPreview(event) {
   });
 }
 
-// Show SNS Share Button
+// SNS 공유버튼 보여주기
 function showShareBtn() {
-  const shareBtn = document.getElementById("shareBtn");
+  const shareBtn = document.getElementById("sns__share__btn");
   shareBtn.style.display = "block";
 }
 
-// Try Again Button
+// 재시도 버튼
 function tryAgainBtn() {
-  let tryAgain = document.querySelector(".tryAgainBtn");
+  let tryAgain = document.querySelector(".tryAgain__btn");
   tryAgain.style.display = "block";
   tryAgain.addEventListener("click", () => {
     location.reload();
@@ -63,52 +63,53 @@ function tryAgainBtn() {
 }
 
 // Teachable Machine
-// the link to your model provided by Teachable Machine export panel
+// 나의 커스텀 티처블 머신 주소
 const URL = "https://teachablemachine.withgoogle.com/models/PMw8cuZzx/";
 
-let model, labelContainer, maxPredictions;
+let model, resultContainer, maxPredictions;
 
-// Load the image model and setup the webcam
+// Teachable Machine 실행하기
 async function init() {
   const modelURL = URL + "model.json";
   const metadataURL = URL + "metadata.json";
 
-  // load the model and metadata
-  // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-  // or files from your local hard drive
+  // 모델과 메타데이터 로드, 모델의 전체 클래스 받아오기
   model = await tmImage.load(modelURL, metadataURL);
   maxPredictions = model.getTotalClasses();
 
-  // append elements to the DOM
-  labelContainer = document.getElementById("label-container");
+  // 결과를 담을 컨테이너
+  resultContainer = document.getElementById("result__container");
   for (let i = 0; i < maxPredictions; i++) {
-    // and class labels
-    labelContainer.appendChild(document.createElement("div"));
+    resultContainer.appendChild(document.createElement("div"));
   }
 }
 
-// predict a uploaded image through the image model
+// 업로드된 이미지 예측하기
 async function predict() {
-  // predict can take in an image, video or canvas html element
-  let image = document.getElementById("preview-image");
+  let image = document.querySelector(".preview__image");
   const prediction = await model.predict(image, false);
+  // prediction안에는 4가지 객체 배열이 들어있다 [{..}{..}{..}{..}]
+  // key : className, probability
+  // className-value : badmen, goodmen, badwomen, goodwomen
+  // probability-vlaue : 실수값 (AI는 업로드된 사진을 분석하여 위 4개 key값에 부여하는 점수)
+
+  // probability(점수)가 높게 나온대로 배열 순서를 재정렬한다.
   prediction.sort(
     (a, b) => parseFloat(b.probability) - parseFloat(a.probability)
   );
-  // Check Gender
+  // 성별 체크
   checkGender();
-  console.log(prediction[0].className);
-  // If User is a male
+  // 남자일 경우
   if (checkGender() === "male") {
     for (let i = 0; i < maxPredictions; i++) {
       if (
         prediction[i].className === "badmen" ||
         prediction[i].className === "goodmen"
       ) {
-        const resultProbability =
-          Math.round(prediction[i].probability.toFixed(2) * 100) + "%";
-        let label;
+      // 실수값으로 나온 결과를 백분율로 바꿔준다.
+        const resultProbability = Math.round(prediction[i].probability.toFixed(2) * 100) + "%";
         let testResult;
+        let label;
         switch (prediction[i].className) {
           case "badmen":
             testResult = "바람꾼";
@@ -121,20 +122,22 @@ async function predict() {
           default:
             testResult = "알수없음";
         }
-        // the Test Main Result to show only one time
-        if (mainResult !== 0) {
-          let result = document.querySelector(".test-result");
-          result.innerText = testResult;
-          result.style.display = "block";
-          var mainResult = 0;
-        }
-        // View a face feature about Men
-        let moreBtn = document.querySelector(".viewMoreMen");
-        moreBtn.style.display = "block";
-        labelContainer.childNodes[i].innerHTML = label + resultProbability;
-        showShareBtn();
-        tryAgainBtn();
+        // 이미지 테스트 결과
+        let result = document.querySelector(".result");
+        result.innerText = testResult;
+        result.style.display = "block";
+
+        // 남자 바람상 특징 확인 버튼
+        let viewMenBtn = document.querySelector(".view__more__men");
+        viewMenBtn.style.display = "block";
+        
+        // 결과 컨테이너에 담아준다.
+        resultContainer.childNodes[i].innerHTML = label + resultProbability;
+        showShareBtn(); // SNS 공유버튼
+        tryAgainBtn();  // 재시도 버튼
       } else {
+        // 만약 0번 인덱스가 badmen 또는 goodmen이 아닐 경우 
+        // 즉 얼굴이 제대로 안나오거나 여자사진이 올라온 경우 재시도 유도
         if (i === 0) {
           alert("얼굴이 잘 나온 다른 사진으로 재시도 해주세요");
           tryAgainBtn();
@@ -144,17 +147,17 @@ async function predict() {
       }
     }
   }
-  // If User is a female
+  // 여자일 경우
   else {
     for (let i = 0; i < maxPredictions; i++) {
       if (
         prediction[i].className === "badwomen" ||
         prediction[i].className === "goodwomen"
       ) {
-        const resultProbability =
-          Math.round(prediction[i].probability.toFixed(2) * 100) + "%";
-        let label;
+        // 실수값으로 나온 결과를 백분율로 바꿔준다.
+        const resultProbability = Math.round(prediction[i].probability.toFixed(2) * 100) + "%";
         let testResult;
+        let label;
         switch (prediction[i].className) {
           case "badwomen":
             testResult = "바람꾼";
@@ -167,18 +170,22 @@ async function predict() {
           default:
             testResult = "알수없음";
         }
-        if (mainResult !== 0) {
-          let result = document.querySelector(".test-result");
-          result.innerText = testResult;
-          result.style.display = "block";
-          var mainResult = 0;
-        }
-        let moreBtn = document.querySelector(".viewMoreWomen");
+        // 이미지 테스트 결과
+        let result = document.querySelector(".result");
+        result.innerText = testResult;
+        result.style.display = "block";
+        
+        // 여자 바람상 특징 확인 버튼
+        let moreBtn = document.querySelector(".view__more__women");
         moreBtn.style.display = "block";
-        labelContainer.childNodes[i].innerHTML = label + resultProbability;
+
+        // 결과를 컨테이너에 담아준다.
+        resultContainer.childNodes[i].innerHTML = label + resultProbability;
         showShareBtn();
         tryAgainBtn();
       } else {
+        // 만약 0번 인덱스가 badmen 또는 goodmen이 아닐 경우 
+        // 즉 얼굴이 제대로 안나오거나 남자사진이 올라온 경우 재시도 유도
         if (i === 0) {
           alert("얼굴이 잘 나온 다른 사진으로 재시도 해주세요");
           tryAgainBtn();
@@ -189,3 +196,4 @@ async function predict() {
     }
   }
 }
+
